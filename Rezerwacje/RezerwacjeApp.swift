4fc6aa48695d5f39b9f0ad3223f256d784294bf2
kamil <kamilpocz2@gmail.com>
2022-12-05 02:58:03 +0100

    Initial Commit

---
+++ Rezerwacje/RezerwacjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RezerwacjeApp.swift
+//  Rezerwacje
+//
+//  Created by Kamil on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RezerwacjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
